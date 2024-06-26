d20c240c4e0309e5d8f01a8713d12c3f99fc2c7f
blankkode <preabvuthy9@gmail.com>
2024-06-26 13:30:06 +0900

    task understanding of bean

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        boolean helloWorldSame = helloWorld1 == helloWorld2;
+        System.out.println("HelloWorld beans " + helloWorldSame);
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        boolean catSame = cat1 == cat2;
+        System.out.println("Cat beans " + catSame);
+
+    }
+}
